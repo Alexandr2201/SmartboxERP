480a6a57f88ae4893fb3155c2ab63fca5fe6e26d
alexandr2201 <alexandr.vika@gmail.com>
2024-07-19 00:18:30 +0200

    add logging

--- erp-server/endpoints/auth.py
+++ erp-server/endpoints/auth.py
@@ -3,6 +3,7 @@ from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identi
 from sqlalchemy.exc import NoResultFound
 from sqlalchemy import text
 from validations.auth_validation import validate_username, validate_password
+from logger import logger
 
 def create_auth_blueprint(Session):
     auth_bp = Blueprint('auth_bp', __name__)
@@ -15,8 +16,10 @@ def create_auth_blueprint(Session):
 
         # Валидация логина и пароля
         if not validate_username(username):
+            logger.error("Invalid username. Username must be between 2 and 255 characters and not contain any special characters")
             return jsonify({"error": "Invalid username. Username must be between 2 and 255 characters and not contain any special characters."}), 400
         if not validate_password(password):
+            logger.error("Invalid password. Password must be between 8 and 255 characters long and contain both upper and lower case letters")
             return jsonify({"error": "Invalid password. Password must be between 8 and 255 characters long and contain both upper and lower case letters."}), 400
 
         # Проверка пользователя в базе данных
@@ -25,11 +28,15 @@ def create_auth_blueprint(Session):
             query = text("SELECT username FROM users WHERE username = :username AND convert_from(password, 'UTF8') = crypt(:password, convert_from(password, 'UTF8'));")
             user = session.execute(query, {'username': username, 'password': password}).fetchone()
             if user is None:
+                logger.error("User not found")
                 raise NoResultFound
 
             access_token = create_access_token(identity=username)
+            logger.info(f'Logged in as: {username} ')
+
             return jsonify(access_token=access_token)
         except NoResultFound:
+            logger.error("Bad username or password")
             return jsonify({"error": "Bad username or password"}), 401
         except Exception as e:
             return jsonify({"error": f"An unexpected error occurred: {str(e)}"}), 500
@@ -40,6 +47,7 @@ def create_auth_blueprint(Session):
     @jwt_required()
     def protected():
         current_user = get_jwt_identity()
+        logger.info(f'Checked user: {current_user}')
         return jsonify(logged_in_as=current_user), 200
 
     return auth_bp
